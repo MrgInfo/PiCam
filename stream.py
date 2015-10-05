26c66a77a76bb0677bbae31df02f5c9f7c0d301a
Groma István <Groma.Istvan@sdadms.hu>
2015-10-05 17:26:33 +0200

    stream as daemon

--- stream.py
+++ stream.py
@@ -124,5 +124,8 @@ if __name__ == '__main__':
         stream_handler = logging.StreamHandler()
         stream_handler.setLevel(logging.DEBUG)
         logger.addHandler(stream_handler)
-        _run()
         print("Press Ctrl-C to exit...")
+        try:
+            _run()
+        except KeyboardInterrupt:
+            pass
