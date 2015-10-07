a4572d7c00760a4d9bcc392dc378ef20ed3439c0
Groma István <wavezone@mrginfo.com>
2015-10-07 02:05:05 +0200

    LSB

--- upload.py
+++ upload.py
@@ -5,7 +5,7 @@
 # Required-Start:    $network $remote_fs
 # Required-Stop:     $network $remote_fs
 # Default-Start:     2 3 4 5
-# Default-Stop:
+# Default-Stop:      0 1 6
 # Short-Description: Dropbox upload daemon
 ### END INIT INFO
 """
